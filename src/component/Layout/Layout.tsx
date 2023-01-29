79851dbd1f2ca2aaa1e4ebfbe5111e26905636ed
devbratkat <devbrat.kar.5@gmail.com>
2023-01-29 17:47:14 +0530

    feature: Loader, fix: Layout

--- src/component/Layout/Layout.tsx
+++ src/component/Layout/Layout.tsx
@@ -1,15 +1,61 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
+import { useLocation } from "react-router";
+import { NavLink } from "react-router-dom";
+import { GlobalContext } from "../../context/GlobalContext";
 import "../../styles/Layout.css";
+import Loader from "../Loader/Loader";
 
 type Props = {
   children: JSX.Element;
 };
 
+let loadingTimeout: any = null;
+
 const Layout: React.FC<Props> = ({ children }) => {
+  const {
+    state: { loading },
+    dispatch,
+  } = useContext(GlobalContext);
+  const location = useLocation().pathname;
+
+  const loader = async () => {
+    await new Promise((resolve) => {
+      loadingTimeout = setTimeout(resolve, 1000);
+    });
+    dispatch({ type: "LOADING", payload: false });
+  };
+
+  useEffect(() => {
+    dispatch({ type: "LOADING", payload: true });
+    loader();
+
+    return () => {
+      clearTimeout(loadingTimeout);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location]);
+
   return (
     <div className="layout">
-      <div className="layout-navbar"></div>
-      <div className="layout-content">{children}</div>
+      <div className="layout-navbar">
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "layout-nav-link active" : "layout-nav-link"
+          }
+          to="/"
+        >
+          Home
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "layout-nav-link active" : "layout-nav-link"
+          }
+          to="/routing-app"
+        >
+          Routing App
+        </NavLink>
+      </div>
+      <div className="layout-content">{loading ? <Loader /> : children}</div>
     </div>
   );
 };
